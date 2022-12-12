da04988006bd4e0f3577aab7dd8a867f77508ba4
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2022-12-11 16:55:39 -0800

    added Authhguard

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthguard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @UseGuards(LocalAuthguard)
+  @Post('login')
+  login(): any {
+    return {};
+  }
+
+  @Get('protected')
   getHello(): string {
     return this.appService.getHello();
   }
