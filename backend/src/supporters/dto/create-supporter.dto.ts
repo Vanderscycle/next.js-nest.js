b2e80c8f82e2334e5afe51964e5499f458c5c253
Henri Vandersleyen <henri-vandersleyen@protonmail.com>
2022-12-19 13:23:47 -0800

    added cypress e2e

--- backend/src/supporters/dto/create-supporter.dto.ts
+++ backend/src/supporters/dto/create-supporter.dto.ts
@@ -1,21 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateSupporterDto {
   @ApiProperty({
     type: String,
     required: true,
   })
+  @IsString()
   name: string;
 
   @ApiProperty({
     type: String,
     required: true,
   })
+  @IsString()
   username: string;
 
   @ApiProperty({
     type: String,
     required: true,
   })
+  @IsString()
   password: string;
 }
---
+++ frontend/cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  // These settings apply everywhere unless overridden
+  defaultCommandTimeout: 5000,
+  viewportWidth: 1920,
+  viewportHeight: 1800,
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.excludeSpecPattern = "**/2-advanced-examples/*.js";
+      return config 
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
